1ec007d0bf841c4e81cb312fa2d3913468ac086d
maxcramer <maximiliancramer@yahoo.com>
2022-06-01 12:19:16 +0100

    input validating url

--- src/components/UrlInput/UrlInput.js
+++ src/components/UrlInput/UrlInput.js
@@ -1,17 +1,35 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 import "./UrlInput.css";
 
 const UrlInput = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({ mode: "onTouched", defaultValues: { url: "https://" } });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <React.Fragment>
       <section>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <h2>Validate URL</h2>
-          <input type="url" />
-          <button type="submit" value="Submit">
-            Validate URL
-          </button>
+          <input
+            type="url"
+            {...register("url", {
+              required: { value: true, message: "URL is Required" },
+              pattern: {
+                value: /^((ftp|http|https):\/\/)?www\.([A-z]+)\.([A-z]{2,})/,
+                message: "Please enter a valid URL",
+              },
+            })}
+          />
+          <input type="submit" value="Validate URL"></input>
+          {errors.url && <span>{errors.url.message}</span>}
         </form>
       </section>
     </React.Fragment>
