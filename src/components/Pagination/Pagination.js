aff9dbfc8dcbcf16895a01e3d5f602445b23cd69
maxcramer <maximiliancramer@yahoo.com>
2022-06-08 14:41:01 +0100

    fixing minor bug on pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -2,7 +2,8 @@ import React from "react";
 
 import "./Pagination.css";
 
-export const Pagination = ({ urlPerPage, totalUrls, paginate, e }) => {
+export const Pagination = ({ urlPerPage, totalUrls, paginate }) => {
+  console.log("Correct version running up to FireBase");
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalUrls / urlPerPage); i++) {
     pageNumbers.push(i);
@@ -11,7 +12,7 @@ export const Pagination = ({ urlPerPage, totalUrls, paginate, e }) => {
     <nav>
       {pageNumbers.map((number) => (
         <li className="page_number" key={number}>
-          <a id="page_number_item" onClick={(e) => paginate(number)} href="#">
+          <a id="page_number_item" onClick={() => paginate(number)} href="#">
             {number}
           </a>
         </li>
