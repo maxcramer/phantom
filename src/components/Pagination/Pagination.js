16979c06c3cdae6e037070747e500d8a10e97b72
maxcramer <maximiliancramer@yahoo.com>
2022-06-08 14:25:32 +0100

    noticed small error where page refresh on pagination click, have fixed

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./Pagination.css";
 
-export const Pagination = ({ urlPerPage, totalUrls, paginate }) => {
+export const Pagination = ({ urlPerPage, totalUrls, paginate, e }) => {
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalUrls / urlPerPage); i++) {
     pageNumbers.push(i);
@@ -11,7 +11,7 @@ export const Pagination = ({ urlPerPage, totalUrls, paginate }) => {
     <nav>
       {pageNumbers.map((number) => (
         <li className="page_number" key={number}>
-          <a id="page_number_item" onClick={() => paginate(number)} href="!#">
+          <a id="page_number_item" onClick={(e) => paginate(number)} href="#">
             {number}
           </a>
         </li>
